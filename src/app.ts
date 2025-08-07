7f040f9f18037e596e24cc2c0b1cc830c61eb6eb
Saad Ibne Omar <86186495+BadgerSIO@users.noreply.github.com>
2025-08-07 15:24:48 +0600

    task 1 completed

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,21 @@
+import cors from "cors";
 import express, { Application, Request, Response } from "express";
+import router from "./app/routes";
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
 
 const app: Application = express();
 // Middleware
 app.use(express.json());
+app.use(cors());
+
+app.use("/api/v1", router);
+// Health check route
 app.get("/", (req: Request, res: Response) => {
   res.send("VoyagePro server running 🥳");
 });
+// Global error handler
+app.use(globalErrorHandler);
+// Not found route
+app.use(notFound);
 export default app;

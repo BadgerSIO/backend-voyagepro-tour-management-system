7b1c5eada62b44593418eb4ec4034b0d019c5bc3
Saad Ibne Omar <86186495+BadgerSIO@users.noreply.github.com>
2025-08-09 12:07:03 +0600

    part 2 complete

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,8 +1,25 @@
 import { Router } from "express";
 import { UserControllers } from "./user.controller";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { createUserZodSchema, updateUserZodSchema } from "./user.validation";
+import { Role } from "./user.interface";
+import { checkAuth } from "../../middlewares/checkAuth";
 const router = Router();
 
-router.post("/register", UserControllers.createUser);
-router.get("/", UserControllers.getAllUsers);
+router.post(
+  "/register",
+  validateRequest(createUserZodSchema),
+  UserControllers.createUser
+);
+router.patch(
+  "/:id",
+  validateRequest(updateUserZodSchema),
+  UserControllers.updateUser
+);
+router.get(
+  "/all-users",
+  checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  UserControllers.getAllUsers
+);
 
 export const UserRoutes = router;
